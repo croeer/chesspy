bcb088822a8bf188c37f273154eee35b264236ae
croeer <christian@roeer.info>
2015-08-30 23:32:05 +0200

    implement coordinate transformation

--- detect.py
+++ detect.py
@@ -8,15 +8,18 @@ Created on Sat Aug 29 18:50:46 2015
 import cv2
 import numpy as np
 import packages.sunfish as sunfish
-import packages.xboard
+import packages.xboard as xboard
 #from matplotlib import pyplot as plt
 
-#pos = packages.xboard.parseFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+pos = xboard.parseFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 
-#move, score = sunfish.search(pos)
+move, score = sunfish.search(pos)
 
-#print move
-#print score
+print move, score
+print("My move:", sunfish.render(move[0]) + sunfish.render(move[1]))
+pos = pos.move(move)
+print(' '.join(pos.board))
+
 
 img_rgb = cv2.imread('samples/all_pieces.png')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
@@ -27,7 +30,7 @@ def getPositionsByTemplateMatching( filename, img ):
     w, h = template.shape[::-1]
     
     res = cv2.matchTemplate(img,template,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.9
+    threshold = 0.8
     loc = np.where( res >= threshold)
     for pt in zip(*loc[::-1]): 
         addPt = True
@@ -61,20 +64,32 @@ def getBoard( img ):
     return cv2.boundingRect(best_cnt)
 
 def transformCoordinatesToField( board, point ):
-    return
+    binSizeX = board[2]//8
+    binSizeY = board[3]//8
 
-print getPositionsByTemplateMatching('templates/black_pawn.png', img_gray)    
+    pointInBoardCoordinates = (point[0]-board[0],point[1]-board[1])
+    
+    binX = -(-pointInBoardCoordinates[0]//binSizeX)
+    binY = -(-pointInBoardCoordinates[1]//binSizeY)
+        
+    return (chr(ord('a')+(binX-1)),chr(ord('1')+(9-binY-1)))
+
+foundPieces = getPositionsByTemplateMatching('templates/white_bishop.png', img_gray)    
 
 img_rgb2=img_rgb.copy()
 x,y,w,h = getBoard(img_gray)
-cv2.rectangle(img_rgb2,(x,y),(x+w,y+h),(0,255,0),2)
-cv2.imwrite('bb.png',img_rgb2)
 
-img_cont=img_rgb.copy()
-cv2.drawContours(img_cont, [best_cnt], 0, (0,255,0), 3)
+for piece in foundPieces:
+    print transformCoordinatesToField((x,y,w,h), piece)
+
+#cv2.rectangle(img_rgb2,(x,y),(x+w,y+h),(0,255,0),2)
+#cv2.imwrite('bb.png',img_rgb2)
+
+#img_cont=img_rgb.copy()
+#cv2.drawContours(img_cont, [best_cnt], 0, (0,255,0), 3)
 
 #img = cv2.drawContours(img_gray, contours, -1, (0,255,0), 3)
-cv2.imwrite('cont.png',img_cont)
+#cv2.imwrite('cont.png',img_cont)
 #cv2.imshow('Image', img_rgb)
 #cv2.waitKey(0)
 
