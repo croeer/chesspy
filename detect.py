d88f20b89331616a364eaf36f4d14f280a477e40
croeer <christian@roeer.info>
2017-01-04 22:31:31 +0100

    do not create debug files

--- detect.py
+++ detect.py
@@ -21,7 +21,7 @@ img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
 img_masked,b = getBoard(img_gray)
 img_board = img_gray[ b[1]:b[1]+b[3] , b[0]:b[0]+b[2] ]
-cv2.imwrite('outboard.png', img_board)
+#cv2.imwrite('outboard.png', img_board)
 
 fen = setupBoard(b, img_board)
 
